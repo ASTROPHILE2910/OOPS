427069214704f427c399bfc52090382957d693ee
Anandita mukherjee <ananditam30@gmail.com>
2024-07-14 11:36:28 +0530

    added

---
+++ binary.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+class Distance{
+    public:
+    int feet;
+    int inches;
+    Distance(){
+        this->feet=0;
+        this->inches=0;
+    }
+    Distance(int f,int i){
+        this->feet=f;
+        this->inches=i;
+
+    }
+    Distance operator + (Distance& d2){
+        Distance d3;
+        d3.feet=this->feet+d2.feet;
+        d3.inches=this->inches+d2.inches;
+        return d3;
+
+    }
+
+};
+int main(){
+    Distance d1(8,9);
+    Distance d2(10,2);
+    Distance d3;
+    d3 = d1+d2;
+    cout<<"total feet and inches:"<<d3.feet<<"'"<<d3.inches<<endl;
+    return 0;
+
+}
